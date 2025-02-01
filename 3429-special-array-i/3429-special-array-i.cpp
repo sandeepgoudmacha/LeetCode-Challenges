b291b47baacc05414386c2887e334bdd8dc0e3f2
sandeepgoudmacha <143279752+sandeepgoudmacha@users.noreply.github.com>
2025-02-01 21:37:41 +0530

    Time: 0 ms (100%), Space: 27.6 MB (61.71%) - LeetHub

---
+++ 3429-special-array-i/3429-special-array-i.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isArraySpecial(vector<int>& nums) {
+        for(int i=0;i<(int)nums.size()-1;i++){
+            if((nums[i]%2==0&&nums[i+1]%2==0) || (nums[i]%2!=0&&nums[i+1]%2!=0)){
+                return 0;
+            }
+        }
+        return 1;
+        
+    }
+};
