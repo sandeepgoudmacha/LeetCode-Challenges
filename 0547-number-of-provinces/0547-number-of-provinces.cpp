a7a6093f2939604cc14ffbff5e6a1f344a40dcdc
sandeepgoudmacha <143279752+sandeepgoudmacha@users.noreply.github.com>
2025-03-27 12:50:58 +0530

    Time: 0 ms (100%), Space: 19.9 MB (53.75%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -4,15 +4,7 @@ public:
 
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n=isConnected.size();
-        vector<vector<int>>v(n);
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(isConnected[i][j] == 1 && i!=j){
-                    v[i].push_back(j);
-                    v[j].push_back(i);
-                }
-            }
-        }
+        
     vector<int> a(n, 0); 
         int c=0;
         for(int i=0;i<n;i++){
@@ -24,10 +16,10 @@ public:
                 while(!q.empty()){
                     int r=q.front();
                     q.pop();
-                    for(auto x: v[r]){
-                        if(!a[x]){
-                            a[x]=1;
-                            q.push(x);
+                    for (int j = 0; j < n; j++) {
+                        if (isConnected[r][j] && !a[j]) {
+                            a[j] = 1;
+                            q.push(j);
                         }
                     }
                 }
