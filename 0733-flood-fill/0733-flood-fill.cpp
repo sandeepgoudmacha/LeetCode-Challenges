ed5d3bb8a1581cc8e5141c38234ca9c0dba1e914
sandeepgoudmacha <143279752+sandeepgoudmacha@users.noreply.github.com>
2025-03-28 19:38:29 +0530

    Time: 4 ms (6.33%), Space: 18.2 MB (59.38%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        // if(image.empty()) return image;
-                int m = image.size(), n = image[0].size();
-       int dx[4] = {0, 0, 1, -1};
-        int dy[4] = {1, -1, 0, 0};
-                queue<pair<int, int>> mat;
-  int initialColor = image[sr][sc];
-        if (initialColor == color) return image;
+        queue<pair<int,int>>q;
+        if(image[sr][sc] == color) return image;
+        q.push({sr,sc});
+        int n=image.size();
+        int m=image[0].size();
+        int dx[4] = {-1,1,0,0};
+        int dy[4] = {0,0,-1,1};
+        int initialColor = image[sr][sc];
         image[sr][sc] = color;
-                mat.push({sr,sc});
-        while(!mat.empty()){
-            int k = mat.size();
-            while(k--){
-                int x = mat.front().first, y = mat.front().second;
-                mat.pop();
-                for(int i = 0; i < 4; ++i){
-                    int nx = x + dx[i], ny = y + dy[i];
-                    if(nx < 0 || ny < 0 || nx >= m || ny >= n || image[nx][ny] != initialColor) continue;
-                    image[nx][ny] = color;
-                    mat.push({nx, ny});
+        while(!q.empty()){
+            int qSize=q.size();
+            while(qSize--){
+                int newX = q.front().first,newY= q.front().second;
+                q.pop();
+                for(int i=0;i<4;i++){
+                    int X = newX+dx[i],Y=newY+dy[i];
+                    if(X <0 || Y<0 || X>=n || Y>=m || image[X][Y] != initialColor ) continue;
+                    image[X][Y] = color;
+                    q.push({X,Y});
                 }
             }
         }
