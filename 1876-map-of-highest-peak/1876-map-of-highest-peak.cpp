9ffde9c8f58ba8503d542e814ddb078e5d654937
sandeepgoudmacha <143279752+sandeepgoudmacha@users.noreply.github.com>
2025-03-29 20:13:30 +0530

    Time: 87 ms (38.7%), Space: 128.3 MB (18.47%) - LeetHub

--- 1876-map-of-highest-peak/1876-map-of-highest-peak.cpp
+++ 1876-map-of-highest-peak/1876-map-of-highest-peak.cpp
@@ -1,36 +1,37 @@
 class Solution {
 public:
-    vector<vector<int>> highestPeak(vector<vector<int>>& isWater) {
-        int n=isWater.size();
-        int m=isWater[0].size();
+    vector<vector<int>> highestPeak(vector<vector<int>>& mat) {
+       int n=mat.size();
+        int m=mat[0].size();
+        // if(mat!=0) return mat;
+      vector<vector<int>>v(n,vector<int>(m,0));
+      vector<vector<int>>r(n,vector<int>(m,0));
         queue<pair<pair<int,int>,int>>q;
-        vector<vector<int>>v(n,vector<int>(m,0));
-        vector<vector<int>>h(n,vector<int>(m,0));
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(isWater[i][j] == 1) {
-                    q.push({{i,j},0});
-                    v[i][j]=1;
-                }
+      for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(mat[i][j] == 1){
+                v[i][j] =1;
+                q.push({{i,j},0});
             }
         }
-        int dx[4]={-1,1,0,0};
-        int dy[4]={0,0,-1,1};
+      }
+      int dx[4]={-1,1,0,0};
+      int dy[4]={0,0,-1,1};
         while(!q.empty()){
-            int indA=q.front().first.first;
-            int indB=q.front().first.second;
-            int cellH=q.front().second;
+            int a=q.front().first.first;
+            int b=q.front().first.second;
+            int distance = q.front().second;
+            r[a][b]=distance;
             q.pop();
-            h[indA][indB]=cellH;
             for(int i=0;i<4;i++){
-                int nx=dx[i]+indA,ny=dy[i]+indB;
-                if(nx>=0 && ny>=0 && nx<n && ny <m && v[nx][ny]==0){
-                    v[nx][ny]=1;
-                    q.push({{nx,ny},cellH+1});
+                int nx=dx[i]+a,ny=dy[i]+b;
+                
+                if(nx>=0 && ny>=0 && nx<n && ny<m && v[nx][ny] == 0) {
+                        q.push({{nx,ny},distance+1});
+                        v[nx][ny]=1;
                 }
             }
-
         }
-        return h;
+        return r;
     }
 };
