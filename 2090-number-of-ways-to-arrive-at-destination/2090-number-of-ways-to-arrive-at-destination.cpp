620d544e180d3ef12e81a68452f4389392634ebc
sandeepgoudmacha <143279752+sandeepgoudmacha@users.noreply.github.com>
2025-05-02 20:01:42 +0530

    Time: 6 ms (62.17%), Space: 35 MB (76.48%) - LeetHub

--- 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.cpp
+++ 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.cpp
@@ -7,24 +7,17 @@ public:
             graph[u].emplace_back(v, time);
             graph[v].emplace_back(u, time);
         }
-
         vector<long long> dist(n, LLONG_MAX);
         vector<int> ways(n, 0);
-
         dist[0] = 0;
         ways[0] = 1;
-
         priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<>> pq;
         pq.emplace(0, 0);
-
         const int MOD = 1e9 + 7;
-
         while (!pq.empty()) {
             auto [d, node] = pq.top();
             pq.pop();
-
             if (d > dist[node]) continue;
-
             for (const auto& [neighbor, time] : graph[node]) {
                 if (dist[node] + time < dist[neighbor]) {
                     dist[neighbor] = dist[node] + time;
@@ -35,7 +28,6 @@ public:
                 }
             }
         }
-
         return ways[n - 1];
     }
 };
