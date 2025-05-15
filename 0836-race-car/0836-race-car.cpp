1fe0d61b7a6e7bc343e007415f7f340e8fbe1cf5
sandeepgoudmacha <143279752+sandeepgoudmacha@users.noreply.github.com>
2025-05-15 20:41:48 +0530

    Time: 1335 ms (10.51%), Space: 276.8 MB (5.69%) - LeetHub

--- 0836-race-car/0836-race-car.cpp
+++ 0836-race-car/0836-race-car.cpp
@@ -1,29 +1,45 @@
 class Solution {
 public:
     int racecar(int target) {
-        queue<pair<int,pair<int,int>>> q;
-        set<pair<int,int>>vis;
-        q.push({0,{1,0}});
-        vis.insert({0,1});
-        while(!q.empty()){
-            auto top=q.front(); q.pop();
-            int p=top.first;
-            int s=top.second.first,c=top.second.second;
-            // cout<<p<<" "<<s<<" "<<c<<endl;
-            if(p==target) 
-                return c;
+        set<vector<int>>v;
+        queue<vector<int>>q;
+        vector<int>a(3);
+        a[0]=0; a[1]=1;
+        a[2]=0;
+        q.push(a);
+        vector<int>b(2);
+        b[0]=0; b[1]=1;
+        v.insert(b);
+        while(!q.empty()) {
+            vector<int>c=q.front();
+            q.pop();
+            int p=c[0],s=c[1],d=c[2];
+            // cout<<p<<" "<<s<<" "<<d<<endl;
+            if(p==target) return d;
             int np=p+s,ns=s*2;
-            pair<int,int> x={np,ns};
-            int rs=s>0?-1:1;
-            // cout<<rs<<endl;
-            pair<int,int> y={p,rs};
-            if(abs(np)<=2*target && !vis.count(x)){
-                vis.insert(x);
-                q.push({np,{ns,c+1}});
+            if(np<=10000 && np>0) {
+                vector<int>e(2);
+                e[0]=np; e[1]=ns;
+                if(v.find(e)==v.end()) {
+                vector<int>f(3);
+                f[0]=np; f[1]=ns;
+                f[2]=d+1;
+                q.push(f);
+                v.insert(e);
+              }
             }
-            if(!vis.count(y)){
-                vis.insert(y);
-                q.push({p,{rs,c+1}});
+            int r;
+            if(s>0) r=-1;
+            else r=1;
+            vector<int>g(2);
+            g[0]=p;
+            g[1]=r;
+            if(v.find(g)==v.end()) {
+                vector<int>h(3);
+                h[0]=p; h[1]=r;
+                h[2]=d+1;
+                q.push(h);
+                v.insert(g);
             }
         }
         return -1;
