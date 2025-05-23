1371f63ed96f3e498b33ce6699157d06cf6d5ebf
sandeepgoudmacha <143279752+sandeepgoudmacha@users.noreply.github.com>
2025-05-23 17:52:30 +0530

    Time: 3 ms (1.68%), Space: 17.5 MB (52.3%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -2,33 +2,13 @@ class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
         int n=nums.size();
-        vector<int>v;
+        vector<int>v(2,-1);
         int i=-1,j=-1;
-        int l=0,r=n-1;
-        while(l<=r){
-            int mid=(l+r)/2;
-            if(nums[mid]>=target){
-                i=mid;
-                r=mid-1;
-            }
-            else{
-                l=mid+1;
-            }
-        }
-        if(i==-1 ||i>=n ||nums[i]!=target) i=-1;
-        l=0,r=n-1;
-        while(l<=r){
-            int mid=(l+r)/2;
-            if(nums[mid]<=target){
-                j=mid;
-                l=mid+1;
-            }
-            else{
-               r=mid-1;
-            }
-        }
-        if(j==-1 ||j<0||nums[j]!=target ) j=-1;
-        v.push_back(i);v.push_back(j);
+        i=find(nums.begin(),nums.end(),target)-nums.begin();
+        if(i == nums.size() || nums[i]!=target) return v;
+        j=nums.rend() -find(nums.rbegin(),nums.rend(),target)-1;
+        v[0]=i;
+        v[1]=j;
         return v;
     }
 };
