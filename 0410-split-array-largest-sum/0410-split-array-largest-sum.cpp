cc5abfbfba36bd3e20222cc5b1a2fb299b869a83
sandeepgoudmacha <143279752+sandeepgoudmacha@users.noreply.github.com>
2025-05-26 18:39:50 +0530

    Time: 0 ms (100%), Space: 10.5 MB (56.56%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -3,6 +3,7 @@ public:
     int sumOfSubArrays(vector<int>& v, int k, int mid, int&mini){
         int currentSum=v[0];
         // int maxi=INT_MIN;
+        mini=INT_MAX;
         int noOfSubArrays=1;
         for(int i=1;i<(int)v.size();i++){
             if(currentSum+v[i]<=mid){
@@ -10,8 +11,9 @@ public:
             }
             else{
                 noOfSubArrays++;
-                currentSum=v[i];
                 mini=max(mini,currentSum);
+                currentSum=v[i];
+                // mini=max(mini,currentSum);
             }
             
         }
@@ -22,21 +24,21 @@ public:
     int splitArray(vector<int>& nums, int k) {
         int n=nums.size();
         int l=*max_element(nums.begin(),nums.end()),r=accumulate(nums.begin(),nums.end(),0);
-        int mini=INT_MAX;
+        //  mini=INT_MAX;
         int maxi=INT_MIN;
         while(l<=r){
             int mid=(l+r)/2;
+           int mini=INT_MAX;
             int sum=sumOfSubArrays(nums, k, mid, mini);
-            
             if(sum<=k){
                 // maxi=max(mini,maxi);
-                mini=mid;
+                maxi=mid;
                 r=mid-1;
             }
             else{
                 l=mid+1;
             }
         }
-        return mini;
+        return maxi;
     }
 };
