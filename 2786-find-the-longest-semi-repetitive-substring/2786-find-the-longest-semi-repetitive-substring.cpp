c5be0caa1e0c6ce937c3b062aafa12c6f227758c
sandeepgoudmacha <143279752+sandeepgoudmacha@users.noreply.github.com>
2025-05-27 20:30:38 +0530

    Time: 0 ms (100%), Space: 10.2 MB (89.64%) - LeetHub

--- 2786-find-the-longest-semi-repetitive-substring/2786-find-the-longest-semi-repetitive-substring.cpp
+++ 2786-find-the-longest-semi-repetitive-substring/2786-find-the-longest-semi-repetitive-substring.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int longestSemiRepetitiveSubstring(string s) {
-         int n = s.size();
-    if (n <= 1) return n;
-    int maxLen = 1, start = 0, adjDupCount = 0;
-    for (int i = 1; i < n; ++i) {
-        if (s[i] == s[i - 1]) adjDupCount++;
-        while (adjDupCount > 1) {
-            if (s[start] == s[start + 1]) adjDupCount--;
-            start++;
+        int n=s.size();
+        if(n<=1) return n;
+        int maxi=1,st=0,a=0;
+        for(int i=1;i<n;i++) {
+            if(s[i]==s[i-1]) a++;
+            while(a>1) {
+                if(s[st]==s[st+1]) a--;
+                st++;
+            }
+            maxi=max(maxi,i-st+1);
         }
-        maxLen = max(maxLen, i - start + 1);
-    }
-    return maxLen;
+    return maxi;
     }
 };
