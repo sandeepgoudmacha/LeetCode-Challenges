139b03a57b4c3be4b7f3c35cf9f24881b82cb1de
sandeepgoudmacha <143279752+sandeepgoudmacha@users.noreply.github.com>
2025-09-12 13:17:21 +0530

    Time: 8 ms (7.57%), Space: 9.3 MB (57.21%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -5,8 +5,8 @@ public:
        map<char,char>m1,m2;
        for(int i=0;i<s.length();i++){
             // if(s[i]==t[i]) continue;
-            if((m1.count(s[i]) && m1[s[i]]==t[i]) || (m2.count(t[i]) && m2[t[i]]==s[i])) continue;
-            else if((m1.count(s[i]) && m1[s[i]]!=t[i]) || (m2.count(t[i]) && m2[t[i]]!=s[i])) return 0;
+            // if((m1.count(s[i]) && m1[s[i]]==t[i]) || (m2.count(t[i]) && m2[t[i]]==s[i])) continue;
+             if((m1.count(s[i]) && m1[s[i]]!=t[i]) || (m2.count(t[i]) && m2[t[i]]!=s[i])) return 0;
             m1[s[i]]=t[i];
             m2[t[i]]=s[i];
        }
