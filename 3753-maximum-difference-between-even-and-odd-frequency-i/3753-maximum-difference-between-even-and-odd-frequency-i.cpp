c3c8459357562f6ce221079472950150d112ed7f
sandeepgoudmacha <143279752+sandeepgoudmacha@users.noreply.github.com>
2025-09-12 23:29:17 +0530

    Time: 0 ms (100%), Space: 9.9 MB (42.79%) - LeetHub

--- 3753-maximum-difference-between-even-and-odd-frequency-i/3753-maximum-difference-between-even-and-odd-frequency-i.cpp
+++ 3753-maximum-difference-between-even-and-odd-frequency-i/3753-maximum-difference-between-even-and-odd-frequency-i.cpp
@@ -7,15 +7,15 @@ public:
             // pq.push_back()
             m[s[i]]++;
         }
-        int mind=INT_MAX,maxd=INT_MIN,mine=INT_MAX,maxe=INT_MIN;
+        int maxd=INT_MIN,mine=INT_MAX;
         for(auto x:m){
             if(x.second % 2==0){
-                if(maxe<x.second) maxe=x.second;
+                // if(maxe<x.second) maxe=x.second;
                 if(mine>x.second) mine=x.second;
             }
             if(x.second % 2!=0){
                 if(maxd<x.second) maxd=x.second;
-                if(mind>x.second) mind=x.second;
+                // if(mind>x.second) mind=x.second;
             }
             // v.push_back(x.second);
         }
