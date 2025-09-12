5a06fff84f7acb58f17225dfd955ebb33ba469bd
sandeepgoudmacha <143279752+sandeepgoudmacha@users.noreply.github.com>
2025-09-12 13:16:32 +0530

    Time: 2 ms (52.82%), Space: 9.4 MB (57.21%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        if(s==t) return 1;
-        unordered_map<char,char>m,m2;
-        for(int i=0;i<s.length();i++){
-            // char x=s[i];
+       if(s==t) return 1;
+       map<char,char>m1,m2;
+       for(int i=0;i<s.length();i++){
             // if(s[i]==t[i]) continue;
-            if(m.find(s[i])!= m.end() && m[s[i]]!=t[i] ||( m2.find(t[i])!=m2.end() && m2[t[i]]!=s[i])) return 0;
-            // s[i]=t[i];
-            m[s[i]]=t[i];
-             m2[t[i]] = s[i];
-        }
-        // cout<<s<<" "<<t;
-        return 1;
+            if((m1.count(s[i]) && m1[s[i]]==t[i]) || (m2.count(t[i]) && m2[t[i]]==s[i])) continue;
+            else if((m1.count(s[i]) && m1[s[i]]!=t[i]) || (m2.count(t[i]) && m2[t[i]]!=s[i])) return 0;
+            m1[s[i]]=t[i];
+            m2[t[i]]=s[i];
+       }
+       return 1;
     }
 };
