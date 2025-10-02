92420bf081b0752747fdb922297aaa21d5da4bc3
sandeepgoudmacha <143279752+sandeepgoudmacha@users.noreply.github.com>
2025-10-02 16:35:29 +0530

    Time: 1 ms (36.1%), Space: 97.4 MB (25.69%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -5,8 +5,12 @@ public:
         int res=INT_MAX,maxi=0;
         if(prices.size()<=1) return 0;
         for(int i=0;i<prices.size();i++){
-             res=min(res,prices[i]);
-            maxi=max(maxi, prices[i]-res);
+          if(prices[i]<res){
+            res=prices[i];
+          }
+          if(maxi<prices[i]-res){
+            maxi=prices[i]-res;
+          }
         }
         return maxi;
     }
