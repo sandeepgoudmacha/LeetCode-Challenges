155d846b2d3b2aa05842829611663cbc025218e1
sandeepgoudmacha <143279752+sandeepgoudmacha@users.noreply.github.com>
2025-11-18 13:25:44 +0530

    Time: 0 ms (100%), Space: 18.5 MB (78.72%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,31 +1,40 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        int m = matrix[0].size();
-        vector<vector<int>> visited = matrix;
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(matrix[i][j] == 0){
-                    for(int k=0; k<m; k++){
-                        visited[i][k] = 0;
+        int n=matrix.size();
+        int m=matrix[0].size();
+        int col=1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+                    if(j!=0){
+                        matrix[0][j]=0;
+                    }
+                    else    
+                        col=0;
+                }
+            }
+        }
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(matrix[i][j]!=0){
+                    if(matrix[i][0]==0|| matrix[0][j]==0){
+                        matrix[i][j]=0;
                     }
                 }
             }
         }
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(matrix[i][j] == 0){
-                    for(int k=0; k<n; k++){
-                        visited[k][j] = 0;
-                    }
-                }
+        if(matrix[0][0]==0){
+            for(int i=0;i<m;i++){
+                matrix[0][i]=0;
             }
         }
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                matrix[i][j] = visited[i][j];
+        if(col==0){
+            for(int i=0;i<n;i++){
+                matrix[i][0]=0;
             }
         }
+        // return matrix;
     }
 };
